1200dbd88d13bb888c053049a52d92e6d22811f4
Paulolima <pmarciol@hotmail.com>
2025-04-30 19:37:40 -0300

    corregdo branch main

--- exercicios_colab/exercio2.py
+++
@@ -1,8 +0,0 @@
-num1 = int(input('Digite o primeiro número:'))
-num2 = int(input('Digite o segundo número:'))
-if num1 > num2:
-  print('O número maior é: ', num1)
-elif num1 == num2:
-  print('Os números são iguais')
-else:
-  print('Onúmero maioe é: ', num2)
