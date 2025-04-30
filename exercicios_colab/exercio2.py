ee5e0abf9f321cf3232db57b03a1ddbe82d145ba
Paulolima <pmarciol@hotmail.com>
2025-04-30 19:14:24 -0300

    exercicio3.py

---
+++ exercicios_colab/exercio2.py
@@ -0,0 +1,8 @@
+num1 = int(input('Digite o primeiro número:'))
+num2 = int(input('Digite o segundo número:'))
+if num1 > num2:
+  print('O número maior é: ', num1)
+elif num1 == num2:
+  print('Os números são iguais')
+else:
+  print('Onúmero maioe é: ', num2)
