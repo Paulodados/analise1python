36683c61bfd8c13fc6a21bf8fe84e49c6a86b612
Paulodados <pmarciol@hotmail.com>
2025-05-06 15:11:35 -0300

    renomear

--- exerciio40.py
+++
@@ -1,5 +0,0 @@
-# Utilizando a função for, if
-lista = [15, 23, 42, 29, 3]
-for numero in lista:
-  if numero % 2 == 0:
-    print(numero)
--- exexrcicio39.py
+++
@@ -1,4 +0,0 @@
-# Utilizando a função .sort
-numeros = [11, 23, 10, 22, 20, 17, 19, 18, 21]
-numeros.sort()
-print('Lista ordenada:', numeros)
